901c5165227914933f37f857424e5eea8f5e5157
mcdonndi <mcdonndi@tcd.ie>
2015-12-04 16:27:53 +0000

    MakeNotification Class functional - Requires use
    Notification class changed to MakeNotification.
    createNotification function added
    Currently not in use

---
+++ app/src/main/java/com/example/android/todolistgh/MakeNotification.java
@@ -0,0 +1,49 @@
+package com.example.android.todolistgh;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+/**
+ * Created by Diarmuid on 30/11/2015.
+ */
+public class MakeNotification {
+    Context context;
+
+    public MakeNotification(Context context)
+    {
+        this.context = context;
+    }
+
+    /**
+     * Creates the notification which directs you to
+     * Main Activity when clicked
+     */
+    public void createNotification () {
+        int mId = 1;
+        Intent resultIntent = new Intent(context, MainActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(
+                0,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("To Do List")
+                .setContentText("Something is due");
+
+
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(mId, mBuilder.build());
+    }
+}
